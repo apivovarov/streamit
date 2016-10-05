c597cf7e194d6d74680bc2858d7f56a148e15797
Alexander Pivovarov <apivovarov@gmail.com>
2016-10-05 10:29:10 -0700

    Add reduceByKeyAndWindow

--- src/main/scala/streamit/ErrorsPrinter.scala
+++ src/main/scala/streamit/ErrorsPrinter.scala
@@ -1,6 +1,7 @@
 package streamit
 
 import org.apache.spark.SparkConf
+import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Milliseconds, StreamingContext}
 
 import scala.util.Try
@@ -8,13 +9,27 @@ import scala.util.Try
 object ErrorsPrinter {
 
   def main(args: Array[String]): Unit = {
+
+    org.apache.log4j.Logger.getLogger("org.apache.spark").setLevel(org.apache.log4j.Level.WARN)
+    org.apache.log4j.Logger.getLogger("akka").setLevel(org.apache.log4j.Level.WARN)
+
     val conf = new SparkConf()
 
-    val ssc = new StreamingContext(conf, Milliseconds(10000))
+    val dur = Milliseconds(10000)
 
-    val ts = ssc.socketTextStream("localhost", 7799).map(s => Try(s.toInt).getOrElse(0))
+    val ssc = new StreamingContext(conf, dur)
+    ssc.checkpoint("/tmp/spark-checkpoint")
 
-    ts.map(x => (if (x >= 0) "p" else "n", x)).reduceByKey(_ + _).print()
+    val ts = ssc.socketTextStream("localhost", 7799, StorageLevel.MEMORY_AND_DISK_SER).
+      map(s => Try(s.toInt).getOrElse(0))
+
+    val keyV = ts.map(x => (if (x >= 0) "p" else "n", x))
+
+    keyV.reduceByKey(_ + _).print()
+
+    //val w = keyV.window(dur * 3)
+    //w.reduceByKey((x, y) => x + y).print()
+    keyV.reduceByKeyAndWindow(_ + _, _ - _, dur * 3).print()
 
     ssc.start()
     ssc.awaitTermination()
